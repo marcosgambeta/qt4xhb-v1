a6c6c34423539cff08ac25d42f6e56e4d75bb9d7
Marcos Antonio Gambeta <marcosgambeta@uol.com.br>
2017-02-20 04:37:27 -0300

    file dummy.prg replaced by dummy.cpp

---
+++ source/dummy.cpp
@@ -0,0 +1,77 @@
+/*
+
+  Qt4xHb - bibliotecas de ligação entre Harbour/xHarbour e Qt Framework 4
+
+  Copyright (C) 2012-2017 Marcos Antonio Gambeta <marcosgambeta@uol.com.br>
+
+*/
+
+/*
+  Funções nulas, para evitar erros com classes ou recursos não implementados
+*/
+
+#include "hbapi.h"
+
+HB_FUNC( Q_IPV6ADDR )
+{
+}
+
+HB_FUNC( QAXAGGREGATED )
+{
+}
+
+HB_FUNC( QEVENTPRIVATE )
+{
+}
+
+HB_FUNC( QITEMEDITORFACTORY )
+{
+}
+
+HB_FUNC( QMATRIX )
+{
+}
+
+HB_FUNC( QMATRIX3X3 )
+{
+}
+
+HB_FUNC( QSTRING )
+{
+}
+
+HB_FUNC( QSTRINGREF )
+{
+}
+
+HB_FUNC( QVARIANTHASH )
+{
+}
+
+HB_FUNC( QXMLSTREAMENTITYDECLARATIONS )
+{
+}
+
+HB_FUNC( QXMLSTREAMNAMESPACEDECLARATIONS )
+{
+}
+
+HB_FUNC( QXMLSTREAMNOTATIONDECLARATIONS )
+{
+}
+
+HB_FUNC( UCHAR )
+{
+}
+
+HB_FUNC( QFONTENGINE )
+{
+}
+
+HB_FUNC( QPLATFORMGLCONTEXT )
+{
+}
+
+HB_FUNC( QDECLARATIVECOMPONENTATTACHED )
+{
+}
