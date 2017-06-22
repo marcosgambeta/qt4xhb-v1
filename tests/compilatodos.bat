856da703e80944c69c4252d3d53daa344b59ea64
Marcos Antonio Gambeta <marcosgambeta@uol.com.br>
2017-06-22 03:55:37 -0300

    compilatodos.bat: updated
    Modified:
    tests/compilatodos.bat

--- tests/compilatodos.bat
+++ tests/compilatodos.bat
@@ -6,62 +6,62 @@ rem
 rem Batch para compilar testes.
 rem
 
-hbmk2 mainwindow
-hbmk2 qboxlayout1
-hbmk2 qboxlayout2
-hbmk2 qboxlayout3
-hbmk2 qboxlayout4
-hbmk2 qcalendarwidget
-hbmk2 qcolordialog1
-hbmk2 qcolordialog2
-hbmk2 qcombobox
-hbmk2 qdateedit
-hbmk2 qdatetimeedit
-hbmk2 qdesktopwidget
-hbmk2 qdial1
-hbmk2 qdial2
-hbmk2 qdialog
-hbmk2 qdockwidget
-hbmk2 qdoublespinbox
-hbmk2 qerrormessage
-hbmk2 qfiledialog1
-hbmk2 qfiledialog2
-hbmk2 qfocusframe
-hbmk2 qfontcombobox
-hbmk2 qfontdialog
-hbmk2 qformlayout
-hbmk2 qgridlayout
-hbmk2 qgroupbox
-hbmk2 qhboxlayout
-hbmk2 qinputdialog
-hbmk2 qlabel
-hbmk2 qlcdnumber
-hbmk2 qlistview
-hbmk2 qmainwindow
-hbmk2 qmdiarea
-hbmk2 qmenu1
-hbmk2 qmenu2
-hbmk2 qmessagebox
-hbmk2 qprintpreviewdialog
-hbmk2 qprogressbar
-hbmk2 qprogressdialog
-hbmk2 qpushbutton1
-hbmk2 qpushbutton2
-hbmk2 qpushbutton3
-hbmk2 qrubberband
-hbmk2 qscrollbar
-hbmk2 qshortcut
-hbmk2 qslider
-hbmk2 qspinbox
-hbmk2 qstatusbar
-hbmk2 qtablewidget1
-hbmk2 qtablewidget2
-hbmk2 qtimeedit
-hbmk2 qtsqldemo
-hbmk2 quiloader
-hbmk2 qvboxlayout
-hbmk2 qwebview
-hbmk2 qwidget
-hbmk2 qwizard
-hbmk2 setfont
-hbmk2 testpdf
+hbmk2 mainwindow          %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qboxlayout1         %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qboxlayout2         %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qboxlayout3         %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qboxlayout4         %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qcalendarwidget     %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qcolordialog1       %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qcolordialog2       %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qcombobox           %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qdateedit           %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qdatetimeedit       %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qdesktopwidget      %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qdial1              %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qdial2              %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qdialog             %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qdockwidget         %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qdoublespinbox      %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qerrormessage       %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qfiledialog1        %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qfiledialog2        %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qfocusframe         %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qfontcombobox       %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qfontdialog         %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qformlayout         %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qgridlayout         %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qgroupbox           %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qhboxlayout         %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qinputdialog        %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qlabel              %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qlcdnumber          %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qlistview           %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qmainwindow         %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qmdiarea            %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qmenu1              %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qmenu2              %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qmessagebox         %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qprintpreviewdialog %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qprogressbar        %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qprogressdialog     %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qpushbutton1        %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qpushbutton2        %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qpushbutton3        %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qrubberband         %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qscrollbar          %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qshortcut           %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qslider             %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qspinbox            %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qstatusbar          %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qtablewidget1       %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qtablewidget2       %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qtimeedit           %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qtsqldemo           %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 quiloader           %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qvboxlayout         %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qwebview            %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qwidget             %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 qwizard             %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 setfont             %1 %2 %3 ..\qt4xhb.hbc
+hbmk2 testpdf             %1 %2 %3 ..\qt4xhb.hbc
