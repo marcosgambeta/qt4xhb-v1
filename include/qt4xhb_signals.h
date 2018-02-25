2bb1a6c59a0cb69e9a11cba508a60f617d361ba4
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2018-02-25 11:01:52 -0300

    include/qt4xhb_signals.h: new include file
    Added:
    include/qt4xhb_signals.h

---
+++ include/qt4xhb_signals.h
@@ -0,0 +1,20 @@
+/*
+
+  Qt4xHb - Bindings libraries for Harbour/xHarbour and Qt Framework 4
+
+  Copyright (C) 2018 Marcos Antonio Gambeta <marcosgambeta AT outlook DOT com>
+
+*/
+
+#ifndef QT4XHB_SIGNALS_H
+#define QT4XHB_SIGNALS_H
+
+bool Signals_connect_signal ( QObject * object, QString signal, PHB_ITEM codeblock );
+bool Signals_disconnect_signal ( QObject * object, QString signal );
+bool Signals_is_signal_connected( QObject * object, QString signal );
+PHB_ITEM Signals_return_codeblock ( QObject * object, QString signal );
+void Signals_release_codeblocks ();
+void Signals_disconnect_all_signals (QObject * obj);
+bool Signals_connection_disconnection ( QObject * s, QString signal, QString slot );
+
+#endif /* QT4XHB_SIGNALS_H */
