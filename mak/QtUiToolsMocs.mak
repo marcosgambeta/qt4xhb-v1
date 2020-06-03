5414db4315d8fe95f47e1cac29661f5b32effbf8
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2020-06-03 18:59:43 -0300

    HUiLoader: new class
    Added:
    source/QtUiTools/HUiLoader.cpp
    source/QtUiTools/HUiLoader.h
    source/QtUiTools/HUiLoaderClass.prg
    tests/HUiLoader.ui
    tests/HUiLoader1.prg
    tests/HUiLoader2.prg
    
    Modified:
    mak/QtUiTools.mak
    mak/QtUiToolsMocs.mak

--- mak/QtUiToolsMocs.mak
+++ mak/QtUiToolsMocs.mak
@@ -3,3 +3,6 @@
 #
 # Copyright (C) 2020 Marcos Antonio Gambeta <marcosgambeta AT outlook DOT com>
 #
+
+$(QT4XHB_OBJ_DIR)/HUiLoaderMoc.cpp : $(QT4XHB_SRC_DIR)/QtUiTools/HUiLoader.h
+	$(MOC) $^ -o $@
---
+++ source/QtUiTools/HUiLoader.h
@@ -0,0 +1,45 @@
+/*
+
+  Qt4xHb - Bindings libraries for Harbour/xHarbour and Qt Framework 4
+
+  Copyright (C) 2020 Marcos Antonio Gambeta <marcosgambeta AT outlook DOT com>
+
+*/
+
+#ifndef HUILOADER_H
+#define HUILOADER_H
+
+#include <QtUiTools/QUiLoader>
+#include <QtGui/QAction>
+#include <QtGui/QLayout>
+
+#include "qt4xhb_common.h"
+#include "qt4xhb_macros.h"
+#include "qt4xhb_utils.h"
+
+class HUiLoader : public QUiLoader
+{
+  Q_OBJECT
+public:
+  HUiLoader( QObject *parent = 0 );
+  ~HUiLoader();
+
+  QAction * createAction( QObject *parent = 0, const QString &name = QString() );
+  QActionGroup * createActionGroup( QObject *parent = 0, const QString &name = QString() );
+  QLayout * createLayout( const QString &className, QObject *parent = 0, const QString &name = QString() );
+  QWidget * createWidget( const QString &className, QWidget *parent = 0, const QString &name = QString() );
+
+private:
+  PHB_ITEM m_createActionBlock;
+  PHB_ITEM m_createActionGroupBlock;
+  PHB_ITEM m_createLayoutBlock;
+  PHB_ITEM m_createWidgetBlock;
+
+public:
+  void setCreateActionCB( PHB_ITEM block );
+  void setCreateActionGroupCB( PHB_ITEM block );
+  void setCreateLayoutCB( PHB_ITEM block );
+  void setCreateWidgetCB( PHB_ITEM block );
+};
+
+#endif /* HUILOADER_H */
