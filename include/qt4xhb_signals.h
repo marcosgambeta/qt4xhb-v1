87080d214c2df1fcec01413bc321ddb9824aea93
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2020-06-03 04:50:19 -0300

    include/qt4xhb_signals.h: updated

--- include/qt4xhb_signals.h
+++ include/qt4xhb_signals.h
@@ -11,14 +11,14 @@
 
 namespace Qt4xHb
 {
-//void Signals_connect_signal ( QObject * object, QString signal, PHB_ITEM codeblock );
-void Signals_disconnect_signal ( QObject * object, QString signal );
-//bool Signals_is_signal_connected ( QObject * object, QString signal );
-PHB_ITEM Signals_return_codeblock ( QObject * object, QString signal );
-void Signals_disconnect_all_signals (QObject * obj, bool children);
-bool Signals_connection_disconnection ( QObject * s, QString signal, QString slot );
-PHB_ITEM Signals_return_object ( void * ptr, const char * classname );
-PHB_ITEM Signals_return_qobject ( QObject * ptr, const char * classname );
+  //void Signals_connect_signal ( QObject * object, QString signal, PHB_ITEM codeblock );
+  void Signals_disconnect_signal ( QObject * object, QString signal );
+  //bool Signals_is_signal_connected ( QObject * object, QString signal );
+  PHB_ITEM Signals_return_codeblock ( QObject * object, QString signal );
+  void Signals_disconnect_all_signals (QObject * obj, bool children);
+  bool Signals_connection_disconnection ( QObject * s, QString signal, QString slot );
+  PHB_ITEM Signals_return_object ( void * ptr, const char * classname );
+  PHB_ITEM Signals_return_qobject ( QObject * ptr, const char * classname );
 }
 
 #endif /* QT4XHB_SIGNALS_H */
