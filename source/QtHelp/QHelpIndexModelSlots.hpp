8571ecdc4ef1a18d953e9448806f2152b8df3a9d
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2023-01-31 23:11:11 -0300

    Update QtHelp

--- source/QtHelp/QHelpIndexModelSlots.hpp
+++ source/QtHelp/QHelpIndexModelSlots.hpp
@@ -19,10 +19,10 @@
 
 #include <QtHelp/QHelpIndexModel>
 
-#include "qt4xhb_common.h"
-#include "qt4xhb_macros.h"
-#include "qt4xhb_utils.h"
-#include "qt4xhb_signals.h"
+#include "qt4xhb_common.hpp"
+#include "qt4xhb_macros.hpp"
+#include "qt4xhb_utils.hpp"
+#include "qt4xhb_signals.hpp"
 
 class QHelpIndexModelSlots: public QObject
 {
