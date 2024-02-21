41e3f24077e3afdb997dc4aa6ab35d9c3e0500f6
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2024-02-21 08:13:13 -0300

    Update QtScript formatting (clang-format)

--- source/QtScript/QScriptEngineSlots.hpp
+++ source/QtScript/QScriptEngineSlots.hpp
@@ -24,14 +24,14 @@
 #include "qt4xhb_utils.hpp"
 #include "qt4xhb_signals.hpp"
 
-class QScriptEngineSlots: public QObject
+class QScriptEngineSlots : public QObject
 {
   Q_OBJECT
-  public:
-  QScriptEngineSlots(QObject * parent = 0);
+public:
+  QScriptEngineSlots(QObject *parent = 0);
   ~QScriptEngineSlots();
-  public slots:
-  void signalHandlerException( const QScriptValue & exception );
+public slots:
+  void signalHandlerException(const QScriptValue &exception);
 };
 
 #endif /* QSCRIPTENGINESLOTS_H */
