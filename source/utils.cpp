513d244d7f84e0b9e19ac809d801e7ad4651d97c
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2025-09-20 08:35:53 -0300

    Update utils.cpp: formatting

--- source/utils.cpp
+++ source/utils.cpp
@@ -20,8 +20,7 @@ void createReturnClass(void *ptr, const char *classname)
 {
   PHB_DYNS pDynSym = hb_dynsymFindName(classname);
 
-  if (pDynSym != NULL)
-  {
+  if (pDynSym != NULL) {
     hb_vmPushDynSym(pDynSym);
     hb_vmPushNil();
     hb_vmDo(0);
@@ -42,8 +41,7 @@ void createReturnClass(const void *ptr, const char *classname)
 {
   PHB_DYNS pDynSym = hb_dynsymFindName(classname);
 
-  if (pDynSym != NULL)
-  {
+  if (pDynSym != NULL) {
     hb_vmPushDynSym(pDynSym);
     hb_vmPushNil();
     hb_vmDo(0);
@@ -64,8 +62,7 @@ void createReturnClass(void *ptr, const char *classname, bool destroy)
 {
   PHB_DYNS pDynSym = hb_dynsymFindName(classname);
 
-  if (pDynSym != NULL)
-  {
+  if (pDynSym != NULL) {
     hb_vmPushDynSym(pDynSym);
     hb_vmPushNil();
     hb_vmDo(0);
@@ -89,8 +86,7 @@ void createReturnClass(const void *ptr, const char *classname, bool destroy)
 {
   PHB_DYNS pDynSym = hb_dynsymFindName(classname);
 
-  if (pDynSym != NULL)
-  {
+  if (pDynSym != NULL) {
     hb_vmPushDynSym(pDynSym);
     hb_vmPushNil();
     hb_vmDo(0);
@@ -114,8 +110,7 @@ void convert_qlist_int_to_array(const QList<int> &list)
 {
   PHB_ITEM pArray = hb_itemArrayNew(0);
   const int count = list.count();
-  for (int i = 0; i < count; i++)
-  {
+  for (int i = 0; i < count; i++) {
     PHB_ITEM pItem = hb_itemPutNI(NULL, list[i]);
     hb_arrayAddForward(pArray, pItem);
     hb_itemRelease(pItem);
@@ -128,8 +123,7 @@ void convert_qlist_qint8_to_array(const QList<qint8> &list)
 {
   PHB_ITEM pArray = hb_itemArrayNew(0);
   const int count = list.count();
-  for (int i = 0; i < count; i++)
-  {
+  for (int i = 0; i < count; i++) {
     PHB_ITEM pItem = hb_itemPutNI(NULL, list[i]);
     hb_arrayAddForward(pArray, pItem);
     hb_itemRelease(pItem);
@@ -142,8 +136,7 @@ void convert_qlist_qint16_to_array(const QList<qint16> &list)
 {
   PHB_ITEM pArray = hb_itemArrayNew(0);
   const int count = list.count();
-  for (int i = 0; i < count; i++)
-  {
+  for (int i = 0; i < count; i++) {
     PHB_ITEM pItem = hb_itemPutNI(NULL, list[i]);
     hb_arrayAddForward(pArray, pItem);
     hb_itemRelease(pItem);
@@ -156,8 +149,7 @@ void convert_qlist_qint32_to_array(const QList<qint32> &list)
 {
   PHB_ITEM pArray = hb_itemArrayNew(0);
   const int count = list.count();
-  for (int i = 0; i < count; i++)
-  {
+  for (int i = 0; i < count; i++) {
     PHB_ITEM pItem = hb_itemPutNI(NULL, list[i]);
     hb_arrayAddForward(pArray, pItem);
     hb_itemRelease(pItem);
@@ -170,8 +162,7 @@ void convert_qlist_qint64_to_array(const QList<qint64> &list)
 {
   PHB_ITEM pArray = hb_itemArrayNew(0);
   const int count = list.count();
-  for (int i = 0; i < count; i++)
-  {
+  for (int i = 0; i < count; i++) {
     PHB_ITEM pItem = hb_itemPutNI(NULL, list[i]);
     hb_arrayAddForward(pArray, pItem);
     hb_itemRelease(pItem);
@@ -184,8 +175,7 @@ void convert_qlist_quint8_to_array(const QList<quint8> &list)
 {
   PHB_ITEM pArray = hb_itemArrayNew(0);
   const int count = list.count();
-  for (int i = 0; i < count; i++)
-  {
+  for (int i = 0; i < count; i++) {
     PHB_ITEM pItem = hb_itemPutNI(NULL, list[i]);
     hb_arrayAddForward(pArray, pItem);
     hb_itemRelease(pItem);
@@ -198,8 +188,7 @@ void convert_qlist_quint16_to_array(const QList<quint16> &list)
 {
   PHB_ITEM pArray = hb_itemArrayNew(0);
   const int count = list.count();
-  for (int i = 0; i < count; i++)
-  {
+  for (int i = 0; i < count; i++) {
     PHB_ITEM pItem = hb_itemPutNI(NULL, list[i]);
     hb_arrayAddForward(pArray, pItem);
     hb_itemRelease(pItem);
@@ -212,8 +201,7 @@ void convert_qlist_quint32_to_array(const QList<quint32> &list)
 {
   PHB_ITEM pArray = hb_itemArrayNew(0);
   const int count = list.count();
-  for (int i = 0; i < count; i++)
-  {
+  for (int i = 0; i < count; i++) {
     PHB_ITEM pItem = hb_itemPutNI(NULL, list[i]);
     hb_arrayAddForward(pArray, pItem);
     hb_itemRelease(pItem);
@@ -226,8 +214,7 @@ void convert_qlist_quint64_to_array(const QList<quint64> &list)
 {
   PHB_ITEM pArray = hb_itemArrayNew(0);
   const int count = list.count();
-  for (int i = 0; i < count; i++)
-  {
+  for (int i = 0; i < count; i++) {
     PHB_ITEM pItem = hb_itemPutNI(NULL, list[i]);
     hb_arrayAddForward(pArray, pItem);
     hb_itemRelease(pItem);
@@ -240,8 +227,7 @@ void convert_qlist_qreal_to_array(const QList<qreal> &list)
 {
   PHB_ITEM pArray = hb_itemArrayNew(0);
   const int count = list.count();
-  for (int i = 0; i < count; i++)
-  {
+  for (int i = 0; i < count; i++) {
     PHB_ITEM pItem = hb_itemPutND(NULL, list[i]);
     hb_arrayAddForward(pArray, pItem);
     hb_itemRelease(pItem);
@@ -254,8 +240,7 @@ void convert_qlist_double_to_array(const QList<double> &list)
 {
   PHB_ITEM pArray = hb_itemArrayNew(0);
   const int count = list.count();
-  for (int i = 0; i < count; i++)
-  {
+  for (int i = 0; i < count; i++) {
     PHB_ITEM pItem = hb_itemPutND(NULL, list[i]);
     hb_arrayAddForward(pArray, pItem);
     hb_itemRelease(pItem);
@@ -268,8 +253,7 @@ void convert_qstringlist_to_array(const QStringList &list)
 {
   PHB_ITEM pArray = hb_itemArrayNew(0);
   const int count = list.count();
-  for (int i = 0; i < count; i++)
-  {
+  for (int i = 0; i < count; i++) {
     PHB_ITEM pItem = hb_itemPutC(NULL, list[i].toLatin1().data());
     hb_arrayAddForward(pArray, pItem);
     hb_itemRelease(pItem);
@@ -329,18 +313,15 @@ void createReturnQObjectClass(QObject *ptr, const char *classname)
 {
   PHB_DYNS pDynSym = NULL;
 
-  if (ptr != NULL)
-  {
+  if (ptr != NULL) {
     pDynSym = hb_dynsymFindName(ptr->metaObject()->className());
   }
 
-  if (pDynSym == NULL)
-  {
+  if (pDynSym == NULL) {
     pDynSym = hb_dynsymFindName(classname);
   }
 
-  if (pDynSym != NULL)
-  {
+  if (pDynSym != NULL) {
     hb_vmPushDynSym(pDynSym);
     hb_vmPushNil();
     hb_vmDo(0);
@@ -361,18 +342,15 @@ void createReturnQObjectClass(const QObject *ptr, const char *classname)
 {
   PHB_DYNS pDynSym = NULL;
 
-  if (ptr != NULL)
-  {
+  if (ptr != NULL) {
     pDynSym = hb_dynsymFindName(ptr->metaObject()->className());
   }
 
-  if (pDynSym == NULL)
-  {
+  if (pDynSym == NULL) {
     pDynSym = hb_dynsymFindName(classname);
   }
 
-  if (pDynSym != NULL)
-  {
+  if (pDynSym != NULL) {
     hb_vmPushDynSym(pDynSym);
     hb_vmPushNil();
     hb_vmDo(0);
@@ -393,18 +371,15 @@ void createReturnQWidgetClass(QWidget *ptr, const char *classname)
 {
   PHB_DYNS pDynSym = NULL;
 
-  if (ptr != NULL)
-  {
+  if (ptr != NULL) {
     pDynSym = hb_dynsymFindName(ptr->metaObject()->className());
   }
 
-  if (pDynSym == NULL)
-  {
+  if (pDynSym == NULL) {
     pDynSym = hb_dynsymFindName(classname);
   }
 
-  if (pDynSym != NULL)
-  {
+  if (pDynSym != NULL) {
     hb_vmPushDynSym(pDynSym);
     hb_vmPushNil();
     hb_vmDo(0);
@@ -425,18 +400,15 @@ void createReturnQWidgetClass(const QWidget *ptr, const char *classname)
 {
   PHB_DYNS pDynSym = NULL;
 
-  if (ptr != NULL)
-  {
+  if (ptr != NULL) {
     pDynSym = hb_dynsymFindName(ptr->metaObject()->className());
   }
 
-  if (pDynSym == NULL)
-  {
+  if (pDynSym == NULL) {
     pDynSym = hb_dynsymFindName(classname);
   }
 
-  if (pDynSym != NULL)
-  {
+  if (pDynSym != NULL) {
     hb_vmPushDynSym(pDynSym);
     hb_vmPushNil();
     hb_vmDo(0);
@@ -467,8 +439,7 @@ bool isObjectDerivedFrom(int numpar, const QString &className)
 {
   PHB_ITEM pItem = hb_param(numpar, HB_IT_OBJECT);
 
-  if (pItem != NULL)
-  {
+  if (pItem != NULL) {
     return hb_clsIsParent(hb_objGetClass(pItem), className.toUpper().toLatin1().data());
   } else {
     return false;
@@ -482,11 +453,9 @@ QStringList convert_array_parameter_to_qstringlist(int numpar)
 
   PHB_ITEM pArray = hb_param(numpar, HB_IT_ARRAY);
 
-  if (pArray != NULL)
-  {
+  if (pArray != NULL) {
     const int nLen = hb_arrayLen(pArray);
-    for (int i = 0; i < nLen; i++)
-    {
+    for (int i = 0; i < nLen; i++) {
       QString temp = QLatin1String(hb_arrayGetCPtr(pArray, i + 1));
       list << temp;
     }
@@ -511,11 +480,9 @@ QVariantList convert_array_parameter_to_qvariantlist(int numpar)
 
   PHB_ITEM pArray = hb_param(numpar, HB_IT_ARRAY);
 
-  if (pArray != NULL)
-  {
+  if (pArray != NULL) {
     const int nLen = hb_arrayLen(pArray);
-    for (int i = 0; i < nLen; i++)
-    {
+    for (int i = 0; i < nLen; i++) {
       list << *static_cast<QVariant *>(hb_itemGetPtr(hb_objSendMsg(hb_arrayGetItemPtr(pArray, i + 1), "POINTER", 0)));
     }
   }
@@ -531,10 +498,8 @@ void convert_qvariantlist_to_array(const QVariantList &list)
   PHB_ITEM pArray = hb_itemArrayNew(0);
 
   const int count = list.count();
-  for (int i = 0; i < count; i++)
-  {
-    if (pDynSym != NULL)
-    {
+  for (int i = 0; i < count; i++) {
+    if (pDynSym != NULL) {
       hb_vmPushDynSym(pDynSym);
       hb_vmPushNil();
       hb_vmDo(0);
@@ -561,15 +526,13 @@ PHB_ITEM returnQModelIndexObject(void *ptr)
 {
   static PHB_DYNS pDynSym = NULL;
 
-  if (pDynSym == NULL)
-  {
+  if (pDynSym == NULL) {
     pDynSym = hb_dynsymFindName("QMODELINDEX");
   }
 
   PHB_ITEM pObject = hb_itemNew(NULL);
 
-  if (pDynSym != NULL)
-  {
+  if (pDynSym != NULL) {
     hb_vmPushDynSym(pDynSym);
     hb_vmPushNil();
     hb_vmDo(0);
@@ -589,15 +552,13 @@ PHB_ITEM returnQVariantObject(void *ptr)
 {
   static PHB_DYNS pDynSym = NULL;
 
-  if (pDynSym == NULL)
-  {
+  if (pDynSym == NULL) {
     pDynSym = hb_dynsymFindName("QVARIANT");
   }
 
   PHB_ITEM pObject = hb_itemNew(NULL);
 
-  if (pDynSym != NULL)
-  {
+  if (pDynSym != NULL) {
     hb_vmPushDynSym(pDynSym);
     hb_vmPushNil();
     hb_vmDo(0);
@@ -617,20 +578,17 @@ PHB_ITEM returnQWidgetObject(QWidget *ptr)
 {
   PHB_DYNS pDynSym = NULL;
 
-  if (ptr != NULL)
-  {
+  if (ptr != NULL) {
     pDynSym = hb_dynsymFindName(ptr->metaObject()->className());
   }
 
-  if (pDynSym == NULL)
-  {
+  if (pDynSym == NULL) {
     pDynSym = hb_dynsymFindName("QWIDGET");
   }
 
   PHB_ITEM pObject = hb_itemNew(NULL);
 
-  if (pDynSym != NULL)
-  {
+  if (pDynSym != NULL) {
     hb_vmPushDynSym(pDynSym);
     hb_vmPushNil();
     hb_vmDo(0);
@@ -650,20 +608,17 @@ PHB_ITEM returnQObjectObject(QObject *ptr)
 {
   PHB_DYNS pDynSym = NULL;
 
-  if (ptr != NULL)
-  {
+  if (ptr != NULL) {
     pDynSym = hb_dynsymFindName(ptr->metaObject()->className());
   }
 
-  if (pDynSym == NULL)
-  {
+  if (pDynSym == NULL) {
     pDynSym = hb_dynsymFindName("QOBJECT");
   }
 
   PHB_ITEM pObject = hb_itemNew(NULL);
 
-  if (pDynSym != NULL)
-  {
+  if (pDynSym != NULL) {
     hb_vmPushDynSym(pDynSym);
     hb_vmPushNil();
     hb_vmDo(0);
