447c99fb86452b9608723a9ffdf30c8c985b0d6f
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2025-11-18 19:50:32 -0300

    Update qt4xhb_macros.hpp: add macro RQPOINTF

--- include/qt4xhb_macros.hpp
+++ include/qt4xhb_macros.hpp
@@ -162,6 +162,7 @@
 #define RQCOLOR(p)                                          Qt4xHb::createReturnClass(p, "QCOLOR", true)
 #define RQBYTEARRAY(p)                                      Qt4xHb::createReturnClass(p, "QBYTEARRAY", true)
 #define RQPOINT(p)                                          Qt4xHb::createReturnClass(p, "QPOINT", true)
+#define RQPOINTF(p)                                         Qt4xHb::createReturnClass(p, "QPOINTF", true)
 
 // conversion from QString to string
 #define QSTRINGTOSTRING(x)                                  static_cast<const char*>(x.toAscii().data())
